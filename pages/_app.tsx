29954d6381078294bae589aa3fef4a2b72e1a426
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2021-06-01 13:25:09 +0700

    Add skeleton for sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Sidebar from '../components/Sidebar'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14">
-      <div className="col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl">Sidebar</div>
-      <div className="col-span-12 bg-white lg:col-span-9 rounded-2xl">
+      <div className="col-span-12 p-4 text-center bg-white lg:col-span-4 rounded-2xl">
+        <Sidebar />
+      </div>
+      <div className="col-span-12 bg-white lg:col-span-8 rounded-2xl">
         <Component {...pageProps} />
       </div>
     </div>
